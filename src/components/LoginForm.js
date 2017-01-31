852bad58662cbeaaa600ee5dc346f7fd1c72d13a
Tania <taniapapazaf@gmail.com>
2017-01-31 23:26:58 +0000

    create and style input component

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { TextInput } from 'react-native';
-import { Button, Card, CardSection } from './common';
+import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
   state= {
@@ -10,10 +9,10 @@ class LoginForm extends Component {
     return (
       <Card>
         <CardSection>
-          <TextInput
+          <Input
+            label='Email'
             value={this.state.text}
             onChangeText={text => this.setState({ text })}
-            style={{ height: 40, width: 100 }}
           />
         </CardSection>
         <CardSection></CardSection>
