322169637bda1c8cce06d586e139094fec48f39c
Tania Papazafeiropoulou <taniapapazaf@gmail.com>
2017-02-01 14:48:49 +0000

    event handler for when login state changes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, CardSection } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+
+  state = { loggedIn: false };
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyAcxIRcSAmBdafuXrch2imRnDtQIZzHL5Q',
@@ -13,12 +16,33 @@ class App extends Component {
       storageBucket: 'reactnative-auth-3f18f.appspot.com',
       messagingSenderId: '293506336045'
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
   }
+
+  renderContent() {
+
+    if (this.state.loggedIn) {
+      return (
+        <CardSection>
+          <Button>Logout</Button>
+        </CardSection>
+      );
+    }
+    return <LoginForm />;
+  }
+
   render() {
     return (
       <View>
         <Header headerText='Authentication' />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
