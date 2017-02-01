f47943b8fda7da42d1032c9279c7f7c9eb7a4588
Tania Papazafeiropoulou <taniapapazaf@gmail.com>
2017-02-01 12:57:31 +0000

    add spinner

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,28 +1,60 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
 import firebase from 'firebase';
-import { Button, Card, CardSection, Input } from './common';
+import { Button, Card, CardSection, Input, Spinner } from './common';
 
 class LoginForm extends Component {
   state= {
     email: '',
     password: '',
-    error: ''
+    error: '',
+    loading: false
   }
 
   onButtonPress() {
     const { email, password } = this.state;
-    this.setState({ error:'' });
+    this.setState({ error: '', loading: true });
 
     firebase.auth()
       .signInWithEmailAndPassword(email, password)
+      .then(this.onLoginSuccess.bind(this))
       .catch(() => {
         firebase.auth()
           .createUserWithEmailAndPassword(email, password)
+          .then(this.onLoginSuccess.bind(this))
           .catch((error) => {
-            this.setState({ error: error.message });
+            this.onLoginFail(error.message);
           });
       });
+  }
+
+  onLoginSuccess() {
+    this.setState({
+      email: '',
+      password: '',
+      error: '',
+      loading: false
+    });
+  }
+
+  onLoginFail(message) {
+    this.setState({
+      error: message,
+      loading: false
+    })
+  }
+
+  renderButton() {
+    if (this.state.loading) {
+      return (
+        <Spinner size='small' />
+      );
+    }
+    return (
+      <Button onPress={this.onButtonPress.bind(this)}>
+        Login
+      </Button>
+    );
   }
 
   render() {
@@ -49,13 +81,11 @@ class LoginForm extends Component {
         </CardSection>
 
         <CardSection>
-          <Button onPress={this.onButtonPress.bind(this)}>
-            Login
-          </Button>
+          {this.renderButton()}
         </CardSection>
 
         <Text style={styles.errorTextStyle}>
-            {this.state.error}
+          {this.state.error}
         </Text>
 
       </Card>
